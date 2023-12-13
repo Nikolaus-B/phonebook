93153881433f6db06c0e6de693242108bd6c0999
Mykola <53128883+Nikolaus-B@users.noreply.github.com>
2023-12-13 17:21:28 +0200

    add hot-toast

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -2,6 +2,7 @@ import { AppBar } from 'components/AppBar/AppBar';
 import { GlobalStyle } from 'components/GlobalStyle';
 import { Loader } from 'components/Loader/Loader';
 import { Suspense } from 'react';
+import { Toaster } from 'react-hot-toast';
 import { Outlet } from 'react-router-dom';
 
 export const AppLayout = () => {
@@ -14,6 +15,7 @@ export const AppLayout = () => {
         </Suspense>
       </main>
       <GlobalStyle />
+      <Toaster position="top-right" reverseOrder={false} />
     </div>
   );
 };
